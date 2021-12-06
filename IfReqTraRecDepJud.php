48cfd15733f43d4f74fce089af4798448d6c27e7
GABRIEL155555555 <84022090+GABRIEL155555555@users.noreply.github.com>
2021-12-06 14:35:10 -0300

    Alterado pelo gabriel

--- IfReqTraRecDepJud.php
+++ IfReqTraRecDepJud.php
@@ -4,7 +4,7 @@
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);k
 
-    $POST = file_get_contents('php://input');
+    $POST = file_get_contents('php://input');l
     $_POST = json_decode($POST, true);
 
     //* 01 - Registra início da execução
